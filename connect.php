f799587fba9e5fe10d9f447cc113ef336fa7f7b7
sujanbabli <kandelsujan070@gmail.com>
2025-05-16 17:41:40 +0930

    connecting login database

--- connect.php
+++ connect.php
@@ -2,5 +2,7 @@
 // Connect to MySQL with username, password, and database name
 $conn = mysqli_connect('localhost', 'root', 'root', 'myshopping_cart') or die("Connection failed: " . mysqli_connect_error());;
 
+$conn = mysqli_connect('localhost', 'root', 'root', 'login') or die("Connection failed: " . mysqli_connect_error());;
+
 
 ?>
